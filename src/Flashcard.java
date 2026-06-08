c238f2576e88192bcab44b26a2068941cb46b336
gabrmiranda <gabomv2005@gmail.com>
2026-06-08 15:37:25 -0600

    .

--- src/Flashcard.java
+++ src/Flashcard.java
@@ -3,30 +3,22 @@ public class Flashcard {
     private String ejercicio;
     private String respuesta;
     private String explicacion;
+    private String tema;
     private String nivel;
 
-    public Flashcard(String ejercicio, String respuesta,
-                     String explicacion, String nivel) {
+    public Flashcard(
+            String ejercicio,
+            String respuesta,
+            String explicacion,
+            String tema,
+            String nivel) {
 
         this.ejercicio = ejercicio;
         this.respuesta = respuesta;
         this.explicacion = explicacion;
+        this.tema = tema;
         this.nivel = nivel;
     }
 
-    public String getEjercicio() {
-        return ejercicio;
-    }
-
-    public String getRespuesta() {
-        return respuesta;
-    }
-
-    public String getExplicacion() {
-        return explicacion;
-    }
-
-    public String getNivel() {
-        return nivel;
-    }
+    // faltan getters
 }
